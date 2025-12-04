f483fa1876390ac3dd49e31d3cff13f329b4eed7
Yuhan Hao <yuhanhao@gmail.com>
2025-12-03 23:30:32 -0800

    clean up obsolete docs (#42)

--- test_hygiene.scm
+++
@@ -1,8 +0,0 @@
-;; Test hygiene with let-syntax
-;; The macro m should capture 'outer because x is in scope when m is defined
-;; Even though x is shadowed by 'inner in the inner let, the macro should see 'outer
-
-(let ((x 'outer))
-  (let-syntax ((m (syntax-rules () ((m) x))))
-    (let ((x 'inner))
-      (m))))
--- test_hygiene_simple.scm
+++
@@ -1,5 +0,0 @@
-;; Simpler test without the test macro
-(let ((x 'outer))
-  (let-syntax ((m (syntax-rules () ((m) x))))
-    (let ((x 'inner))
-      (m))))
